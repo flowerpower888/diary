895d66be37fab3838257a6fd3a1942f886d70365
aaaaaaaaa <aaaaaaaaaaaaaa>
2020-01-18 02:10:41 +0300

    Updates

---
+++ precache-manifest.4dad4f4047f6619dd6f0c0c247f53e0f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81bb7460169637b2239f237223fdb47b",
+    "url": "/diary/index.html"
+  },
+  {
+    "revision": "73d62a4f619d5ccbe204",
+    "url": "/diary/static/css/main.53b48f63.chunk.css"
+  },
+  {
+    "revision": "3c255ffac32aa8b5e755",
+    "url": "/diary/static/js/2.50b98d90.chunk.js"
+  },
+  {
+    "revision": "73d62a4f619d5ccbe204",
+    "url": "/diary/static/js/main.85bc1f37.chunk.js"
+  },
+  {
+    "revision": "c7d9650bfd2fe594e4cf",
+    "url": "/diary/static/js/runtime-main.6f1a61fa.js"
+  },
+  {
+    "revision": "18f8b21f03f75a5f7e33b457f6c34329",
+    "url": "/diary/static/media/261910552003202.18f8b21f.png"
+  },
+  {
+    "revision": "d75caa1f00d645455da15ac4d6fb3bef",
+    "url": "/diary/static/media/295962751228211.d75caa1f.png"
+  },
+  {
+    "revision": "8ac194b96a15e66acb2ed7cec4b7eedf",
+    "url": "/diary/static/media/296601226182211.8ac194b9.png"
+  },
+  {
+    "revision": "fa20b6ffb39490b8ef404cd4242c579f",
+    "url": "/diary/static/media/297359063039211.fa20b6ff.png"
+  },
+  {
+    "revision": "2bf4f1e30f96c36bada00f82c3aa0831",
+    "url": "/diary/static/media/303432332011211.2bf4f1e3.png"
+  },
+  {
+    "revision": "eea054bf721ccd17bfafc2d8bf836a4c",
+    "url": "/diary/static/media/310868699136211.eea054bf.png"
+  },
+  {
+    "revision": "e023c6256924637bc2567764fe453b6b",
+    "url": "/diary/static/media/311835845046211.e023c625.png"
+  }
+]);
